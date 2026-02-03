964ac9983be1e4ab0911d4756049da57fa5a1d46
Utkarsh Maddheshiya <utkarshmaddheshiya1729@gmail.com>
2026-02-03 13:04:10 +0530

    Update solution for sample-var-std

--- sample-var-std/sample-var-std.py
+++ sample-var-std/sample-var-std.py
@@ -8,11 +8,7 @@ def sample_var_std(x):
     x = np.asarray(x)
     mean_x = np.mean(x)
     n = x.shape[0]
-    sum = 0
-    for i in range(n):
-        sum += (x[i] -mean_x)**2
-
-    var = (1/(n-1))*sum
+    var = np.sum((x-mean_x)**2)/(n-1)
     std = np.sqrt(var)
     return var , std
 
