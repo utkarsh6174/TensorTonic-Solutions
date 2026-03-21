313e90f22aa9cba167a3e59ddfa7112058aa5b0a
Utkarsh Maddheshiya <utkarshmaddheshiya1729@gmail.com>
2026-03-21 17:03:49 +0530

    Add solution for rnn-hidden-state

---
+++ rnn/rnn-hidden-state/rnn-hidden-state.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def init_hidden(batch_size: int, hidden_dim: int) -> np.ndarray:
+    """
+    Initialize the hidden state for an RNN.
+    """
+    # YOUR CODE HERE
+    
+    return np.zeros((batch_size,hidden_dim))
+    pass
